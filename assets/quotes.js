220a3c86aa2c33c40b07b5175cdbbeb38f66ad0b
maulanaguntur <gunturalharits28@gmail.com>
2020-05-06 07:19:43 +0700

    first project

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
+    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "standinshd", quotes: "Hey! I want chuuuu! UwU" },
+    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "I'm very lucky to have you." },
+];
---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Risa", "Babe", "Sayang", "Risakuu", "Risa kesayangan maul"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
