bb67b03985c3956b86e780cd1c1a7fa5969b62a8
Guntur <49824858+maulanaguntur@users.noreply.github.com>
2020-05-06 07:32:59 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -5,6 +5,6 @@ export default [
     { author: "standinshd", quotes: "Hey! I want chuuuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "I love you more than my internet." },
     { author: "standinshd", quotes: "I'm very lucky to have you." },
 ];
